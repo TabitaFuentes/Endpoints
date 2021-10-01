0eaa8510c98e49ee9d14dd2c21bc28e6f277a9e9
TabitaFuentes <tabita.fuentes99@gmail.com>
2021-10-01 16:21:45 -0700

    Endpoints

--- server.js
+++ server.js
@@ -20,57 +20,71 @@ app.all('*',function(req,res,next){
     next();
 });
 
-//THE ACTUAL ENDPOINT
+//THE ACTUAL ENDPOINT (listo)
 app.get('/currentday',function(req,res){
     res.send('1st, october, 2021');
+
 });
 
+//listo
 app.get('/fullname',function(req,res){
     res.send('Tabita Sarai Fuentes Martinez');
+
 });
 
+//listo
 app.get('/city',function(req,res){
     res.send('Playas de Rosarito, B. C.');
+    
 });
 
+//listo
 app.get('/school',function(req,res){
     res.send('Universidad de Mexicali');
+
 });
 
+//listo
 app.post('/sum2numbers',function(req,res){
+   let num = req.body.num;
+   let num2 = req.body.num2;
+   let sum = parseInt(num)+parseInt(num2);
+
+    //create an if sentence (falta)
+    res.send (sum.toString());
+
+});
+
+//listo
+app.post('/multiplication3numbers',function(req,res){
     let n1 = req.body.n1;
     let n2= req.body.n2;
-    let result = n1 + n2
-
+    let result = n1 * n2;
+    
     //create an if sentence 
     res.send(result.toString());
 
-
 });
 
-app.post('/multiplication3numbers',function(req,res){
-    let n1 = req.body.n1;
-    
-    //create an if sentence 
-    res.send(n1)
-
-
-});
-
+//listo
 app.post('/areasquare',function(req,res){
-    let n1 = req.body.n1;
+    let lado = req.body.lado;
+    let lado2 = req.body.lado2;
+    let area = lado * lado2;
     
     //create an if sentence 
-    res.send(n1)
-
+    res.send(area.toString());
 
 });
 
+//Listo
 app.post('/areatriangle',function(req,res){
-    let n1 = req.body.n1;
-    
+    var b = req.body.b;
+    var h = req.body.h;
+    let area = (parseInt(b) * parseInt(h)) / 2;
+
     //create an if sentence 
-    res.send(n1)
+    res.send(area.toString());
 
 
 });
